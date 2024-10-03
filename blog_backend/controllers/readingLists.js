59cf9c78cfd959de7a4e92af90ae3a993ba86cd0
Howard <howard@brightpage.tech>
2024-10-03 16:26:33 +0100

    Exercise 13.22

--- blog_backend/controllers/readingLists.js
+++ blog_backend/controllers/readingLists.js
@@ -1,9 +1,20 @@
 const router = require('express').Router()
 const { ReadingList } = require('../models')
+const { tokenExtractor } = require('../util/middleware')
 
 router.post('/', async (req, res) => {
-    const list = await ReadingList.create(req.body)
-    res.json(list)
+    const list_item = await ReadingList.create(req.body)
+    res.json(list_item)
+})
+
+router.put('/:id', tokenExtractor, async (req, res) => {
+    const list_item = await ReadingList.findByPk(req.params.id)
+    if (list_item.userId !== req.decodedToken.id) {
+        res.status(401).json('Unauthorized')
+    }
+    list_item.blogRead = req.body.blogRead
+    await list_item.save()
+    res.json(list_item)
 })
 
 module.exports = router
